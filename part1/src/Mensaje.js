5adb5afbe315133bb9cde2008e5a8d4b6758f70b
DarkoneSeinen <144294703+DarkoneSeinen@users.noreply.github.com>
2025-01-10 00:48:17 +0000

    lesson 1 added

--- part1/src/Mensaje.js
+++ part1/src/Mensaje.js
@@ -1,5 +1,6 @@
-const Mensaje =() => {
-    return <h1>Hola Funciona desde modulo</h1>
+const Mensaje =(paraps) => { // deben ser parámetros, para que sean reutilizables
+    return <h1 style ={{color : paraps.color }}>
+        {paraps.message}</h1>
 }
 
 export default Mensaje;
