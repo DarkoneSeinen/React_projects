7db4486ec7cfd1cd5d286784a491948ffcab0772
DarkoneSeinen <144294703+DarkoneSeinen@users.noreply.github.com>
2025-06-05 21:26:40 +0000

    course init

--- JavaScript files/variables.js
+++
@@ -1,9 +0,0 @@
-const x = 1 // no se puede cambiar
-let y = 5 // define una variable normal
-
-console.log(x, y)   // se imprime 1, 5
-y += 10
-console.log(x, y)   // se imprime 1, 15
-y = 'sometext'
-console.log(x, y)   // se imprime 1, sometext
-//x = 4               // provoca un error
--- part1/src/Mensaje.js
+++
@@ -1,6 +0,0 @@
-const Mensaje =(paraps) => { // deben ser parámetros, para que sean reutilizables
-    return <h1 style ={{color : paraps.color }}>
-        {paraps.message}</h1>
-}
-
-export default Mensaje;
