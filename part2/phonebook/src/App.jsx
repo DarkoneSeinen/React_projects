95e97c6e42a312fd8c1072cd380ed82c3c0f5962
DarkoneSeinen <144294703+DarkoneSeinen@users.noreply.github.com>
2025-06-08 20:24:22 +0000

    db.json

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
   const [filter, setFilter] = useState('')
 
   // useEffect para obtener datos del servidor
-  useEffect(() => {
+  useEffect(() => { // colocar puerto publico
     axios
       .get('https://literate-space-trout-979xvx5x4xgwf7vp5-3001.app.github.dev/persons')
       .then(response => {
