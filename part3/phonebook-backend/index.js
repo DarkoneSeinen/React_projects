0fc3644c3b9746c05e106eecdc69f3214505059a
DarkoneSeinen <144294703+DarkoneSeinen@users.noreply.github.com>
2025-06-09 19:48:52 +0000

    exercise 3.6

---
+++ part3/phonebook-backend/index.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+  { id: 1, name: "Arto Hellas", number: "040-123456" },
+  { id: 2, name: "Ada Lovelace", number: "39-44-5323523" },
+  { id: 3, name: "Dan Abramov", number: "12-43-234345" },
+  { id: 4, name: "Mary Poppendieck", number: "39-23-6423122" }
+]
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(p => p.id === id)
+  person ? res.json(person) : res.status(404).end()
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(p => p.id !== id)
+  res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+
+  // Verificar campos obligatorios
+  if (!body.name || !body.number) {
+    return res.status(400).json({ error: 'Name or number missing' })
+  }
+
+  // Verificar unicidad del nombre
+  const nameExists = persons.some(p => p.name === body.name)
+  if (nameExists) {
+    return res.status(400).json({ error: 'Name must be unique' })
+  }
+
+  const person = {
+    id: Math.floor(Math.random() * 1000000),
+    name: body.name,
+    number: body.number,
+  }
+
+  persons.push(person)
+  res.json(person)
+})
+
+app.get('/info', (req, res) => {
+  const date = new Date()
+  res.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${date}</p>
+  `)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
