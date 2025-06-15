5eeedd51a9c808fab4733b13ba420db39db9d224
DarkoneSeinen <144294703+DarkoneSeinen@users.noreply.github.com>
2025-06-15 19:21:17 +0000

    exercise 4.2

---
+++ part4/bloglist-backend/test/list_helper.test.js
@@ -0,0 +1,9 @@
+import { test } from 'node:test'
+import assert from 'node:assert'
+import { dummy } from '../utils/list_helper.js'
+
+test('dummy returns one', () => {
+  const blogs = []
+  const result = dummy(blogs)
+  assert.strictEqual(result, 1)
+})
---
+++ part4/bloglist-backend/utils/list_helper.js
@@ -0,0 +1,4 @@
+// utils/list_helper.js
+export const dummy = (blogs) => {
+  return 1
+}
