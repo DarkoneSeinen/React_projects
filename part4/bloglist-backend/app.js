f957b6b4e7b7f549947b1320771526a50509ae05
DarkoneSeinen <144294703+DarkoneSeinen@users.noreply.github.com>
2025-06-17 20:40:11 +0000

    test

--- part4/bloglist-backend/app.js
+++ part4/bloglist-backend/app.js
@@ -4,6 +4,8 @@ import cors from 'cors'
 import blogsRouter from './controllers/blogs.js'
 import config from './utils/config.js'
 import logger from './utils/logger.js'
+const usersRouter = require('./controllers/users')
+
 
 mongoose.set('strictQuery', false)
 
@@ -22,5 +24,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+
 
 export default app
