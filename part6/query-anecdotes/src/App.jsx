4c8c3c1639e71cbd572cc158108f852595dd5e65
DarkoneSeinen <144294703+DarkoneSeinen@users.noreply.github.com>
2025-06-29 19:53:22 +0000

    exercise 6.22

---
+++ part6/query-anecdotes/src/App.jsx
@@ -0,0 +1,14 @@
+import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
+
+const App = () => {
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+      <AnecdoteForm />
+      <AnecdoteList />
+    </div>
+  )
+}
+
+export default App
---
+++ part6/query-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,44 @@
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getAnecdotes, updateAnecdote } from '../requests'
+
+const AnecdoteList = () => {
+  const queryClient = useQueryClient()
+
+  const updateAnecdoteMutation = useMutation({
+    mutationFn: updateAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+    }
+  })
+
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+    retry: 1
+  })
+
+  if (result.isLoading) return <div>Loading...</div>
+  if (result.isError) return <div>Anecdote service not available due to problems in server</div>
+
+  const anecdotes = result.data
+
+  const handleVote = (anecdote) => {
+    updateAnecdoteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 })
+  }
+
+  return (
+    <div>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default AnecdoteList
