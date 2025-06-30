d64bfafafbb234525ef667daa2aad81fce1dfc5e
DarkoneSeinen <144294703+DarkoneSeinen@users.noreply.github.com>
2025-06-30 02:45:14 +0000

    exercise 6.24

--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,44 +1,38 @@
-import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
-import { getAnecdotes, updateAnecdote } from '../requests'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { createAnecdote } from '../requests'
+import { useNotification } from '../NotificationContext'
 
-const AnecdoteList = () => {
+const AnecdoteForm = () => {
   const queryClient = useQueryClient()
+  const [_, dispatch] = useNotification()
 
-  const updateAnecdoteMutation = useMutation({
-    mutationFn: updateAnecdote,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+  const newAnecdoteMutation = useMutation({
+    mutationFn: createAnecdote,
+    onSuccess: (newAnecdote) => {
+      const anecdotes = queryClient.getQueryData(['anecdotes'])
+      queryClient.setQueryData(['anecdotes'], anecdotes.concat(newAnecdote))
+      dispatch({ type: 'SHOW', payload: `anecdote '${newAnecdote.content}' created` })
+      setTimeout(() => dispatch({ type: 'HIDE' }), 5000)
+    },
+    onError: (error) => {
+      dispatch({ type: 'SHOW', payload: error.response.data.error })
+      setTimeout(() => dispatch({ type: 'HIDE' }), 5000)
     }
   })
 
-  const result = useQuery({
-    queryKey: ['anecdotes'],
-    queryFn: getAnecdotes,
-    retry: 1
-  })
-
-  if (result.isLoading) return <div>Loading...</div>
-  if (result.isError) return <div>Anecdote service not available due to problems in server</div>
-
-  const anecdotes = result.data
-
-  const handleVote = (anecdote) => {
-    updateAnecdoteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 })
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    newAnecdoteMutation.mutate({ content, votes: 0 })
   }
 
   return (
-    <div>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => handleVote(anecdote)}>vote</button>
-          </div>
-        </div>
-      )}
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input name="anecdote" />
+      <button type="submit">create</button>
+    </form>
   )
 }
 
-export default AnecdoteList
+export default AnecdoteForm
--- part6/query-anecdotes/src/requests.js
+++ part6/query-anecdotes/src/requests.js
@@ -2,11 +2,20 @@ import axios from 'axios'
 
 const baseUrl = 'https://literate-space-trout-979xvx5x4xgwf7vp5-3001.app.github.dev/anecdotes'
 
-export const getAnecdotes = () =>
-  axios.get(baseUrl).then(res => res.data)
+export const getAnecdotes = async () => {
+  const response = await axios.get(baseUrl)
+  return response.data
+}
 
-export const createAnecdote = (newAnecdote) =>
-  axios.post(baseUrl, newAnecdote).then(res => res.data)
+export const createAnecdote = async (newAnecdote) => {
+  if (newAnecdote.content.length < 5) {
+    throw new Error('too short anecdote, must have length 5 or more')
+  }
+  const response = await axios.post(baseUrl, newAnecdote)
+  return response.data
+}
 
-export const updateAnecdote = (updatedAnecdote) =>
-  axios.put(`${baseUrl}/${updatedAnecdote.id}`, updatedAnecdote).then(res => res.data)
+export const updateAnecdote = async (anecdote) => {
+  const response = await axios.put(`${baseUrl}/${anecdote.id}`, anecdote)
+  return response.data
+}
